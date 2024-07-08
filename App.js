875b21b18b9815180697ac31c29013904a64f827
VagnerBomJesus <vagneripg@gmail.com>
2024-07-08 09:17:13 +0100

    add log in assets

--- App.js
+++ App.js
@@ -1,20 +1,105 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { useState } from "react"
+
+import { View, Text, StyleSheet, Image, TouchableOpacity } from "react-native"
+import Slider from "@react-native-community/slider"
+
+let charset ="abcdefghijklmnopkrstuvxwyzABCDEFGHIJKLMNOPKRSTUVWXYZ0123456789"
 
 export default function App() {
+
+  const [size, setSize] = useState(10)
+
+  function generatePassword (){
+
+    let password = ""
+
+
+    console.log("Clicou-- aqui!");
+  }
+
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+    <View style={styles.contaner}>
+      <Image
+        source={require("./src/assets/logo.png")}
+        style={styles.logo}
+      />
+
+      <Text
+        style={styles.title}
+      > {size} Characters </Text>
+
+
+      <View style={styles.area}>
+
+        <Slider
+          style={{ height: 50, }}
+          minimumValue={6}
+          maximumValue={20}
+          maximumTrackTintColor="#ff0000"
+          minimumTrackTintColor="#392de9"
+          thumbTintColor="#392de9"
+          value={size}
+          onValueChange={
+            (value) => setSize(value.toFixed(0))
+          }
+
+
+
+
+        />
+
+
+
+
+      </View>
+
+      <TouchableOpacity style={styles.button} onPress={generatePassword}>
+        <Text style={styles.textButton}>
+          Generate Password
+        </Text>
+      </TouchableOpacity>
+
     </View>
-  );
+  )
 }
 
+
 const styles = StyleSheet.create({
-  container: {
+  contaner: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    backgroundColor: "#F3F3F3",
+    justifyContent: "center",
+    alignItems: "center"
   },
+  logo: {
+    marginBottom: 60,
+  },
+  title: {
+    fontSize: 30,
+    fontWeight: "bold",
+  },
+  area: {
+    marginTop: 16,
+    marginBottom: 16,
+    width: "80%",
+    backgroundColor: "#fff",
+    borderRadius: 8,
+    padding: 6,
+
+  },
+  button: {
+    backgroundColor: "#392de9",
+    width: "80%",
+    height: 50,
+    alignItems: "center",
+    justifyContent: "center",
+    borderRadius: 8,
+    marginBottom: 18,
+  },
+
+  textButton: {
+    color: "#fff",
+    fontSize: 20,
+    fontWeight: "500"
+  }
 });
